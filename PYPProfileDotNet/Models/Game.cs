33d0b04011fc5159873664d96c56205604fb65ee
benasour <benasour7@gmail.com>
2013-05-06 08:59:44 -0500

    adding node and asp.net communication

--- PYPProfileDotNet/Models/Game.cs
+++ PYPProfileDotNet/Models/Game.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace PYPProfileDotNet.Models
 {
@@ -7,5 +9,32 @@ namespace PYPProfileDotNet.Models
         [Key]
         public int GameId { get; set; }
         public string Name { get; set; }
+    }
+
+    public class GameOptions
+    {
+        public static List<SelectListItem> CoinSides
+        {
+            get
+            {
+                return new List<SelectListItem> {
+                    new SelectListItem { Text = "Heads", Value = "Heads" },
+                    new SelectListItem { Text = "Tails", Value = "Tails" }
+                };
+            }
+        }
+
+        public static List<SelectListItem> HorseSuits
+        {
+            get
+            {
+                return new List<SelectListItem> {
+                    new SelectListItem { Text = "Spades", Value = "Spades" },
+                    new SelectListItem { Text = "Hearts", Value = "Hearts" },
+                    new SelectListItem { Text = "Clubs", Value = "Clubs" },
+                    new SelectListItem { Text = "Diamonds", Value = "Diamonds" }
+                };
+            }
+        }
     }
 }
